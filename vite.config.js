5bcd2ac791611ffc7f38c167b24efbbceef5f21e
hirorogo <hiro114514@proton.me>
2025-10-24 11:23:21 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TicketSystem/",
-  optimizeDeps: {
-    include: ["pdfjs-dist/build/pdf.worker.min.js"],
+  base: '/TicketSystem/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, '404.html'), // 404.htmlを生成
+      },
+    },
   },
 });
